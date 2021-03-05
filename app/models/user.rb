a11ef43a666304189e74255fe0c98d4dcef57a49
Jarfsoft <andresjex@hotmail.com>
2021-03-04 23:59:29 -0600

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 6, maximum: 25 }
 end
