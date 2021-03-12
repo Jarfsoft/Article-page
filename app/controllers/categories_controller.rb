38c2c4aaa1f1dd165df492fd32d3f2e0846dc21e
Jarfsoft <andresjex@hotmail.com>
2021-03-12 13:26:03 -0600

    Add styles to nav-bar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,14 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :priority)
+  end
 end
